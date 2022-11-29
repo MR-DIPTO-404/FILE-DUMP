1e48ae3d6585d4f5584db0fe97970c379beb90d4
MR.DIPTO <118310303+MR-DIPTO-404@users.noreply.github.com>
2022-11-29 14:04:09 +0600

    Update DUMP.py

--- DUMP.py
+++ DUMP.py
@@ -1,8 +1,16 @@
 import os,time
+os.system('git pull')
+
 os.system('clear')
+
 print('  ENJOY UNLIMITED FILE DUMP & SEPARATE FILE TOOLS 🔥')
+
 time.sleep(5)
+
 os.system("xdg-open https://www.facebook.com/REMEMBER.THIS.NAME.MR.DIPTO")
+
 time.sleep(1)
+
 import MR_DIPTO
+
 MR_DIPTO.MR()
